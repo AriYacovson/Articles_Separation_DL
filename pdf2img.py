a92e3b0970823ab852de32f0f7752cd7139d18b4
AriYacobson <ariyacovson@gmail.com>
2023-06-05 15:05:42 +0300

    change pdf2img.py to be a function

--- pdf2img.py
+++ pdf2img.py
@@ -1,27 +1,42 @@
 import os
 from pdf2image import convert_from_path
 
-# Directory containing the PDF files
-pdf_directory = r'Articles_Examples'
+def directory_open_and_convert_pdf_to_png(input_directory_path, poppler_path,
+                                          first_pages_directory_path, other_pages_directory_path):
+    """
+    gets a directory of pdf files, converting them into png
+    and saving them to two classes - first_pages and other_pages
+    :param input_directory_path:
+    :param poppler_path:
+    :param first_pages_directory_path:
+    :param other_pages_directory_path:
+    :type input_directory_path: string
+    :type first_pages_directory_path: string
+    :type other_pages_directory_path: string
+    :return:
+    """
 
-# Get a list of all PDF files in the directory
-pdf_files = [file for file in os.listdir(pdf_directory) if file.endswith('.pdf')]
+    # Directory containing the PDF files
+    pdf_directory = input_directory_path
 
-# Loop through each PDF file
-for pdf_file in pdf_files:
-    # Create the full path to the PDF file
-    pdf_path = os.path.join(pdf_directory, pdf_file)
+    # Get a list of all PDF files in the directory
+    pdf_files = [file for file in os.listdir(pdf_directory) if file.endswith('.pdf')]
 
-    # Convert the PDF file to images
-    images = convert_from_path(
-        pdf_path,
-        grayscale=True,
-        poppler_path=r"C:\Users\ariya\Downloads\Release-23.05.0-0\poppler-23.05.0\Library\bin"
-    )
+    # Loop through each PDF file
+    for pdf_file in pdf_files:
+        # Create the full path to the PDF file
+        pdf_path = os.path.join(pdf_directory, pdf_file)
 
-    # Loop through each image and save it
-    for i, image in enumerate(images):
-        if i == 0:
-            image.save(os.path.join('labeled_data/first_pages', pdf_file + '_page_' + str(i + 1) + '.png'), 'PNG')
-        else:
-            image.save(os.path.join('labeled_data/other_pages', pdf_file + '_page_' + str(i + 1) + '.png'), 'PNG')
+        # Convert the PDF file to images
+        images = convert_from_path(
+            pdf_path,
+            grayscale=True,
+            poppler_path=poppler_path
+        )
+
+        # Loop through each image and save it
+        for i, image in enumerate(images):
+            if i == 0:
+                image.save(os.path.join(first_pages_directory_path, pdf_file + '_page_' + str(i + 1) + '.png'), 'PNG')
+            else:
+                image.save(os.path.join(other_pages_directory_path, pdf_file + '_page_' + str(i + 1) + '.png'), 'PNG')
