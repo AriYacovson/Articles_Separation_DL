43b098c5997967467151c1edf2eb55f29e1989f2
Ari Yacobson <ariyacovson@gmail.com>
2023-06-13 10:48:53 +0300

    fixing the resize to fit the input layer of our model

--- preprocessing.py
+++ preprocessing.py
@@ -3,15 +3,15 @@ from PIL import Image
 import numpy as np
 
 
-def open_resize_and_label_png_directory(input_directory_path, image_width, image_height):
+def open_resize_and_label_png_directory(input_directory_path, image_height, image_width):
     """
     gets a directory of png files, resizing them into the desired size
     :param input_directory_path:
-    :param image_width:
     :param image_height:
+    :param image_width:
     :type input_directory_path: string
-    :type image_width: integer
     :type image_height: integer
+    :type image_width: integer
     :return: images
     :return: labels
     :rtype images: list
@@ -30,10 +30,13 @@ def open_resize_and_label_png_directory(input_directory_path, image_width, image
             image = Image.open(image_path)
 
             # Resize the image
-            image = image.resize((image_width, image_height))
+            image = image.resize((image_height, image_width))
+            image = np.array(image)
+            image = image.reshape((image_height, image_width, 1))
 
             # Normalize the pixel values to [0, 1]
             image = np.array(image) / 255.0
+
 
             # Append the image and label to the respective lists
             images.append(image)
